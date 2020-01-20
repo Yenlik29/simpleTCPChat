af77a0e4143dceb254e4471bc81fb2cc6d2a31d2
Yenlik Bokina <ybokina@Yenliks-MacBook-Air.local>
2020-01-20 14:59:08 +0600

    done

--- server.go
+++ server.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"net"
 	"fmt"
+	"time"
+	"strings"
 	"reflect"
 )
 
@@ -11,6 +13,7 @@ func (Chat *TCPChat) Remove(client *client) {
 	Chat.mutex.Lock()
 	defer Chat.mutex.Unlock()
 
+	name := client.name
 	for i, check := range Chat.clients {
 		if check == client {
 			Chat.clients = append(Chat.clients[:i], Chat.clients[i+1:]...)
@@ -19,21 +22,43 @@ func (Chat *TCPChat) Remove(client *client) {
 
 	fmt.Println("=====Closing connection=====")
 	client.conn.Close()
+
+	for _, client := range Chat.clients {
+		curTime := time.Now()
+		timing := curTime.Format("2006-01-02 15:04:05")
+		msg := "\n[LEFT]:" + name + "\n[" + timing + "][" + client.name + "]:"
+		client.writer.writer.Write([]byte(msg))
+	}
 }
 
-func (Chat *TCPChat) BroadCast(command interface{}) error {
+func (Chat *TCPChat) BroadCast(command interface{}, client *client) error {
 	for _, client := range Chat.clients {
 		if client.name != command.(MessageCommand).Name {
-			client.writer.Write(command)
+			blank := strings.TrimSpace(command.(MessageCommand).Message) == ""
+			if !blank{
+				client.writer.Write(command, client)
+			}
+		} else if client.name == command.(MessageCommand).Name {
+			client.Prefix()
 		}
 		fmt.Println("===", client.name, command.(MessageCommand).Name)
-		// client.writer.Write(command)
 	}
 	return nil
 }
 
 func (Chat *TCPChat) SetName(command string, client *client) error {
 	client.name = command[:len(command)-1]
+	for _, user := range Chat.clients {
+		if user.name == "" {
+			continue
+		} else if user.name != client.name {
+			curTime := time.Now()
+			timing := curTime.Format("2006-01-02 15:04:05")
+			msg := "\n" + client.name + " has joined our chat...\n[" + timing + "]" + "[" + user.name + "]:"
+			user.writer.writer.Write([]byte(msg))
+		}
+	}
+	client.Prefix()
 	return nil
 }
 
@@ -75,7 +100,7 @@ func (Chat *TCPChat) Listen(address string) error {
 }
 
 func (client *client) WelcomeMessage() error {
-	msg := "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--' hjm\n"
+	msg := "Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n"
 	msg = msg + "[ENTER YOUR NAME]: "
 	if err := client.writer.WriteString(msg); err != nil {
 		return err
@@ -84,7 +109,9 @@ func (client *client) WelcomeMessage() error {
 }
 
 func (client *client) Prefix() error {
-	msg := "[MESSAGE]: "
+	curTime := time.Now()
+	timing := curTime.Format("2006-01-02 15:04:05")
+	msg := "[" + timing + "]" + "[" + client.name + "]:"
 	if err := client.writer.WriteString(msg); err != nil {
 		return err
 	}
@@ -100,21 +127,15 @@ func (Chat *TCPChat) Serve(client *client) {
 
 	client.WelcomeMessage()
 	for {
-		if count != 0 {
-			client.Prefix()
-		}
-		fmt.Printf("[COUNT]:[%d][%s]\n", count, client.name)
 		msg, err := cmd.Read(count)
 		if err != nil && err != io.EOF {
 			fmt.Println("[ERROR]:", err)
 		}
 		if msg != nil {
 			if reflect.TypeOf(msg).String() == "main.SendCommand" && count != 0 {
-				go Chat.BroadCast(MessageCommand{Name: client.name, Message: msg.(SendCommand).Message})
+				go Chat.BroadCast(MessageCommand{Name: client.name, Message: msg.(SendCommand).Message}, client)
 			} else if reflect.TypeOf(msg).String() == "main.SendCommand" && count == 0 {
 				go Chat.SetName(msg.(SendCommand).Message, client)
-			} else if reflect.TypeOf(msg).String() == "main.NameCommand" {
-				client.name = msg.(NameCommand).Name
 			}
 		}
 		if err == io.EOF {
--- writer.go
+++ writer.go
@@ -14,7 +14,7 @@ func (w *CommandWriter) WriteString(msg string) error {
 	return err
 }
 
-func (w *CommandWriter) Write(command interface{}) error {
+func (w *CommandWriter) Write(command interface{}, client *client) error {
 	var name string
 
 	if command.(MessageCommand).Name == "" {
@@ -25,7 +25,7 @@ func (w *CommandWriter) Write(command interface{}) error {
 
 	curTime := time.Now()
 	timing := curTime.Format("2006-01-02 15:04:05")
-	msg := "[" + timing + "]" + "[" + name + "]:" + command.(MessageCommand).Message
+	msg := "\n[" + timing + "][" + name + "]:" + command.(MessageCommand).Message + "[" + timing + "][" + client.name + "]:"
 	if err := w.WriteString(msg); err != nil {
 		return err
 	}
